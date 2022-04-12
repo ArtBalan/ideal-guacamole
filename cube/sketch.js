69f2c94374968450be9a3854f68ebb1ce9c56c5c
Arthur Balandreau <artbalan20@gmail.com>
2022-04-12 15:58:27 +0200

    3d stuff finished ?

--- cube/sketch.js
+++ cube/sketch.js
@@ -1,88 +1,79 @@
 /**
- * Vector class to store point information, and calculate its projection on the screen
- * Might add methods for rotation and translation
+ * Simple vector class with basic functionnality
  */
 class Vect {
   /**
-   * Do i need to describe what it is
-   * @param {int} x
-   * @param {int} y
-   * @param {int} z
+   *
+   * @param {int} x x component
+   * @param {int} y y component
+   * @param {int} z z component
    */
-  constructor( x, y, z ) {
+  constructor(x, y, z) {
     this.x = x;
     this.y = y;
     this.z = z;
   }
-
   /**
-   *
-   * @param {int} d projection plane z = d
-   * @param {int} fov field of view of the camera
-   * @param {int} h screen height in pixel
-   * @param {int} w screen widht in pixel
-   * @returns
+   * Project the point onto the screen
+   * Does not draw it but calculate its projected x and y coordinate
+   * @param {int} d projection of the point on the plane z=d
+   * @param {int} fov field of view the camera
    */
-  projectPoint( d, fov, h, w ) {
+  projectPoint(d, fov) {
     // unit projection onto the plane z=d
     let xf = (d * this.x) / this.z;
     // to pixel convetion
-    xf *= h / ((Math.sin(fov) * d) / Math.sin(90 - fov));
+    xf *= HEIGHT / ((Math.sin(fov) * d) / Math.sin(90 - fov));
     // setting to the propper origin
-    xf += h / 2;
-
+    xf += HEIGHT / 2;
     // unit projection onto the plane z=d
     let yf = (d * this.y) / this.z;
     // to pixel convetion
-    yf *= w / ((Math.sin(fov) * d) / Math.sin(90 - fov));
+    yf *= WIDTH / ((Math.sin(fov) * d) / Math.sin(90 - fov));
     // setting to the propper origin
-    yf += w / 2;
+    yf += WIDTH / 2;
 
     return new Vect(xf, yf, 1);
   }
-
   /**
-   * translate the point with the given vector 
-   * @param {int} vect 
+   * Translate the point with the given vector
+   * @param {Vect} vect
    */
-  translatePoint( vect ){
+  translatePoint(vect) {
     this.x += vect.x;
     this.y += vect.y;
     this.z += vect.z;
   }
-
   /**
-   * scale the point from the given origin, and by the scalling vector
+   * Scale the point with the given vector and origin
+   * @param {Vect} f vector containig the 3 axis scalling factor
    * @param {Vect} origin
-   * @param {Vect} f vector containing the 3 axis scalling factor
    */
-  scalePoint( f, origin) {
+  scalePoint(f, origin) {
     this.x = (this.x - origin.x) * f.x + origin.x;
-    this.y = (this.y - origin.y) * f.y + origin.y; 
-    this.z = (this.z - origin.z) * f.z + origin.z; 
+    this.y = (this.y - origin.y) * f.y + origin.y;
+    this.z = (this.z - origin.z) * f.z + origin.z;
   }
   /**
-   * rotate a point around another one based on a plan 
-   * @param {int} a first origin axis coordinate ex: origin.x
-   * @param {int} b second origin axis coordinate ex: origin.y
-   * @param {int} c first point axis coordinate ex: point.x
-   * @param {int} d second point axis coordinate ex: point.y
-   * @param {int} angle 
-   * @returns 
+   * Rotate a point around a given origin and angle
+   * @param {int} a first orgin coordinate ex: origin.x
+   * @param {int} b second origin coordinate ex:origin.y
+   * @param {int} c first point coordinate ex: point.x
+   * @param {int} d second point coordinate ex: point.y
+   * @param {int} angle of rotation
    */
-  planarRotate( a, b, c, d, angle ){
+  planarRotate(a, b, c, d, angle) {
     let out = [];
-    out.push( (Math.cos(angle)*(c-a)) - (Math.sin(angle)*(d-b)) + a );
-    out.push( (Math.sin(angle)*(c-a)) + (Math.cos(angle)*(d-b)) + b );
+    out.push(Math.cos(angle) * (c - a) - Math.sin(angle) * (d - b) + a);
+    out.push(Math.sin(angle) * (c - a) + Math.cos(angle) * (d - b) + b);
     return out;
   }
-
   /**
-   * Rotate a point around another one with the given vector
-   * @param {Vect} origin point of rotation 
-   * @param {Vect} f vector containing the 3 rotations factor
+   * Rotate a point around a given origin and angles
+   * @param {Vect} f vector containing the 3 angles of rotation
+   * @param {Vect} origin origin of rotation
    */
-  rotatePoint( f, origin ){
+  rotatePoint(f, origin) {
     // planar rotate on x
     let rx = this.planarRotate(origin.y, origin.z, this.y, this.z, f.x);
     this.y = rx[0];
@@ -98,67 +89,68 @@ class Vect {
   }
 }
 
-class Item{
+/**
+ * Simple item class
+ */
+class Item {
   /**
-   * 
-   * @param {Vect} center item center 
+   *
+   * @param {Vect} center of the item
+   * @param {int} r radius of the item
+   * @param {string} color color of the item
    */
-  constructor( center, r ){
+  constructor(center, r, color) {
     this.center = center;
     this.r = r;
     this.vectList = [];
+    this.color = color;
   }
-
   /**
-  * Translate the object
-  * @param {Vect} f Translate Vector
-  */
-  translateItem( f ){
-    this.vectList.forEach(e => e.translatePoint(f)); 
+   * Translate the item with the given vector
+   * @param {Vect} f
+   */
+  translateItem(f) {
+    this.vectList.forEach((e) => e.translatePoint(f));
     this.center.translatePoint(f);
   }
-
   /**
-  * Scale the object 
-  * @param {Vect} f Scalling vector
-  * @param {Vect} origin Scalling origin vector
-  */
-  scaleItem( f, origin ){ 
-    this.vectList.forEach(e => e.scalePoint( f , origin));
-    this.center.scalePoint( f , origin);
-  }
-
-  
-  /**
-   * Rotate the given object
-   * @param {Vect} f Rotation Vector
-   * @param {Vect} origin Rotation origin Vector
+   * Scale the item with the given vector and origin
+   * @param {Vect} f vector containig the 3 axis calling factor
+   * @param {*} origin
    */
-  rotateItem( f, origin){
-    this.vectList.forEach(e => e.rotatePoint( f , origin));
-    this.center.rotatePoint( f , origin);
+  scaleItem(f, origin) {
+    this.vectList.forEach((e) => e.scalePoint(f, origin));
+    this.center.scalePoint(f, origin);
   }
-
+  /**
+   * Rotate the item around a given origin and angles
+   * @param {Vect} f vector containing the 3 angles of rotation
+   * @param {Vect} origin origin of rotation
+   */
+  rotateItem(f, origin) {
+    this.vectList.forEach((e) => e.rotatePoint(f, origin));
+    this.center.rotatePoint(f, origin);
+  }
 }
 
-
 /**
- * A simple cube class
- * and methods to drawn it on the screen
+ * Simple cube class
  */
 class Cube extends Item {
   /**
    *
-   * @param {Vect} center Postion of the cube center
-   * @param {int} r Radius of the cube, each edge will be equal to 2 times r
+   * @param {Vect} center of the cube
+   * @param {int} r radius of the cube
+   * @param {color} color of the cube
+   * @param {bool} diag if the diagonal lines are displayed
    */
-  constructor( center, r ) {
-    super(center, r);
+  constructor(center, r, color, diag) {
+    super(center, r, color);
     this.cubeInit();
+    this.diag = diag;
   }
-
   /**
-   * Initalise the point of the cube
+   * Initialise the point of the cube
    */
   cubeInit() {
     this.vectList = [];
@@ -172,16 +164,13 @@ class Cube extends Item {
     let h = new Vect( this.center.x + this.r, this.center.y - this.r, this.center.z + this.r );
     this.vectList.push(a, b, c, d, e, f, g, h);
   }
-
   /**
-   * Draw the cube on the screen
+   * Draw the cube
    */
   draw() {
+    stroke(this.color);
     let projectedList = [];
-    this.vectList.forEach((e) =>
-      projectedList.push(e.projectPoint(d, fov, height, width))
-    );
-
+    this.vectList.forEach((e) => projectedList.push(e.projectPoint(d, fov)));
     let junctionList = [
       [0, 1],
       [1, 2],
@@ -196,84 +185,267 @@ class Cube extends Item {
       [6, 2],
       [3, 7],
     ];
-
     // Diagonals
-    junctionList.push([3,1],[1,6],[6,4],[4,3],[3,6],[4,1]);
-
-    // Draw lines
-    junctionList.forEach((e) =>
-      line( projectedList[e[0]].x, projectedList[e[0]].y, projectedList[e[1]].x, projectedList[e[1]].y )
-    );
+    if (this.diag)
+      junctionList.push([3, 1], [1, 6], [6, 4], [4, 3], [3, 6], [4, 1]);
+    // Index printing
+    // for (let i = 0; i < projectedList.length; i++) { text(i,projectedList[i].x,projectedList[i].y); }
+    junctionList.forEach((e) => { line( projectedList[e[0]].x, projectedList[e[0]].y, projectedList[e[1]].x, projectedList[e[1]].y );});
   }
-
 }
 
-class Sphere extends Item{
-  constructor( center, r, segment){
-    super(center, r);
+/**
+ * Simple sphere class
+ */
+class Sphere extends Item {
+  /**
+   *
+   * @param {Vect} center of the sphere
+   * @param {int} r radius of the sphere
+   * @param {int} segment number of rings
+   * @param {string} color of the sphere
+   */
+  constructor(center, r, segment, color) {
+    super(center, r, color);
     this.segment = segment;
     this.sphereInit();
   }
-
-  sphereInit(){
-    let angle = 360 / ( this.segment ) ;
-    for(let i=0; i < this.segment; i++){
-
-      let tempPoint = new Vect( (Math.cos(angle*i)*this.r)+this.center.x, (Math.sin(angle*i)*this.r)+this.center.y, this.center.z );
-
+  /**
+   * Create a circle of radius this.r
+   */
+  circleInit() {
+    let angle = (Math.PI * 2) / this.segment;
+    for (let i = 0; i < this.segment; i++) {
+      let tempPoint = new Vect(
+        Math.cos(angle * i) * this.r + this.center.x,
+        this.center.y,
+        Math.sin(angle * i) * this.r + this.center.z
+      );
       this.vectList.push(tempPoint);
     }
   }
-
-  draw() {
-    let projectedList = [];
-    this.vectList.forEach((e) =>
-      projectedList.push(e.projectPoint(d, fov, height, width))
-    );
-
-    for (let i = 0; i < projectedList.length-1 ; i++) {
-      line(projectedList[i].x,projectedList[i].y,projectedList[i+1].x,projectedList[i+1].y);      
+  /**
+   * Initialise the point of the sphere
+   */
+  sphereInit() {
+    let angle = (Math.PI * 2) / this.segment;
+    for (let i = 0; i < this.segment / 2; i++) {
+      this.rotateItem(new Vect(angle, 0, 0), this.center);
+      this.circleInit();
     }
-    projectedList.forEach(e => point(e.x, e.y))
-
   }
-
-
+  /**
+   * Draw the sphere
+   */
+  draw() {
+    stroke(this.color);
+    let projectedList = [];
+    this.vectList.forEach((e) => projectedList.push(e.projectPoint(d, fov)));
+    for (let i = 0; i < projectedList.length - 1; i++) {
+      if (i < this.segment && i != 0) {
+        line( projectedList[i].x, projectedList[i].y, projectedList[projectedList.length - i].x, projectedList[projectedList.length - i].y );
+      }
+      if (i < projectedList.length - this.segment) {
+        line( projectedList[i].x, projectedList[i].y, projectedList[i + this.segment].x, projectedList[i + this.segment].y );
+      }
+      // Index printing
+      // text(i,projectedList[i].x,projectedList[i].y);
+      line( projectedList[i].x, projectedList[i].y, projectedList[i + 1].x, projectedList[i + 1].y );
+    }
+  }
 }
 
+/**
+ * Simple cylinder class
+ */
+class Cylinder extends Item {
+  /**
+   *
+   * @param {Vect} center of the cylinder
+   * @param {int} r radius of the cylinder
+   * @param {int} segment number of segments in the rings
+   * @param {int} height of the cylinder
+   * @param {*} hSegment number of rings
+   * @param {*} color of the cylinder
+   */
+  constructor(center, r, segment, height, hSegment, color) {
+    super(center, r, color);
+    this.height = height;
+    this.segment = segment;
+    this.hSegment = hSegment;
+    this.cylinderInit();
+  }
+  /**
+   * Create a circle of radius this.r and offset it on the idk axis
+   * @param {int} offset of the circle compared to the cylinder center
+   */
+  circleInit(offset) {
+    let angle = (Math.PI * 2) / this.segment;
+    for (let i = 0; i < this.segment; i++) {
+      let tempPoint = new Vect(
+        Math.cos(angle * i) * this.r + this.center.x,
+        this.center.y + offset,
+        Math.sin(angle * i) * this.r + this.center.z
+      );
+      this.vectList.push(tempPoint);
+    }
+  }
+  /**
+   * Initialise the point of the cylinder
+   */
+  cylinderInit() {
+    for (
+      let i = -this.height;
+      i <= this.height;
+      i += (this.height / this.hSegment) * 2
+    ) {
+      this.circleInit(i);
+    }
+  }
+  /**
+   * Draw the cylinder
+   */
+  draw() {
+    stroke(this.color);
+    let projectedList = [];
+    this.vectList.forEach((e) => projectedList.push(e.projectPoint(d, fov)));
+    for (let i = 0; i < projectedList.length - 1; i++) {
+      if (i == 0) {
+        line( projectedList[i].x, projectedList[i].y, projectedList[i + this.segment - 1].x, projectedList[i + this.segment - 1].y );
+      }
+      if ((i + 1) % this.segment == 0) {
+        line( projectedList[i + 1].x, projectedList[i + 1].y, projectedList[i + this.segment].x, projectedList[i + this.segment].y );
+      } else {
+        line( projectedList[i].x, projectedList[i].y, projectedList[i + 1].x, projectedList[i + 1].y );
+      }
+      if (i < projectedList.length - this.segment) {
+        line( projectedList[i].x, projectedList[i].y, projectedList[i + this.segment].x, projectedList[i + this.segment].y );
+      }
+      // Index printing
+      // text(i,projectedList[i].x,projectedList[i].y);
+    }
+  }
+}
 
-let width = 600;
-let height = 600;
+/**
+ * Simple cone class
+ */
+class Cone extends Item {
+  /**
+   *
+   * @param {Vect} center of the cone
+   * @param {int} r radius of the base of the cone
+   * @param {int} segment number of segments in the rings
+   * @param {int} height of the cone
+   * @param {int} hSegment number of rings
+   * @param {*} color of the cone
+   */
+  constructor(center, r, segment, height, hSegment, color) {
+    super(center, r, color);
+    this.height = height;
+    this.segment = segment;
+    this.hSegment = hSegment;
+    this.coneInit();
+  }
+  /**
+   * Create a circle of radius tempR and offset it on the idk axis
+   * @param {int} offset of the circle compared to the cone center
+   * @param {int} tempR radius of the circle to generate
+   */
+  circleInit(tempR, offset) {
+    let angle = (Math.PI * 2) / this.segment;
+    for (let i = 0; i < this.segment; i++) {
+      let tempPoint = new Vect(
+        Math.cos(angle * i) * tempR + this.center.x,
+        this.center.y + offset,
+        Math.sin(angle * i) * tempR + this.center.z
+      );
+      this.vectList.push(tempPoint);
+    }
+  }
+  /**
+   * Initialise the point of the cone
+   */
+  coneInit() {
+    let tempR = 0;
+    let reduceFactor = this.r / this.hSegment;
+    for (
+      let i = -this.height;
+      i <= this.height;
+      i += (this.height / this.hSegment) * 2
+    ) {
+      this.circleInit(tempR, i);
+      tempR += reduceFactor;
+    }
+  }
+   /**
+   * Draw the cone
+   */
+  draw() {
+    stroke(this.color);
+    let projectedList = [];
+    this.vectList.forEach((e) => projectedList.push(e.projectPoint(d, fov)));
+    for (let i = 0; i < projectedList.length - 1; i++) {
+      if (i == 0) {
+        line( projectedList[i].x, projectedList[i].y, projectedList[i + this.segment - 1].x, projectedList[i + this.segment - 1].y );
+      }
+      if ((i + 1) % this.segment == 0) {
+        line( projectedList[i + 1].x, projectedList[i + 1].y, projectedList[i + this.segment].x, projectedList[i + this.segment].y );
+      } else {
+        line( projectedList[i].x, projectedList[i].y, projectedList[i + 1].x, projectedList[i + 1].y );
+      }
+      if (i < projectedList.length - this.segment) {
+        line( projectedList[i].x, projectedList[i].y, projectedList[i + this.segment].x, projectedList[i + this.segment].y );
+      }
+      // Index printing
+      // text(i,projectedList[i].x,projectedList[i].y);
+    }
+  }
+}
+
+let WIDTH = 550;
+let HEIGHT = 550;
 
 let d = 2;
 let fov = 10;
 
-// let myCube = new Cube(new Vect(0, 0, 10), 1);
-let mySphere = new Sphere(new Vect(0, 0, 10), 1, 8);
-
-let rate = 1.01;
+let myCube = new Cube(new Vect(0, 0, 10), 1.5, "blue", false);
+let mySphere = new Sphere(new Vect(0, 0, 10), 2.5, 20, "purple");
+let myCylinder = new Cylinder(new Vect(0,0,10), 1, 8, 1, 4, "green");
+let myCone = new Cone(new Vect(0,0,10), 1.4, 5, 1.2, 5,"red");
 
 function setup() {
-  createCanvas(width, height);
+  createCanvas(WIDTH, HEIGHT);
   background(25);
-  stroke('purple'); // Change the color
-  strokeWeight(4);
+  fill("red");
+  stroke("purple"); // Change the color
+  strokeWeight(2);
 }
 
 function draw() {
-  // reset the screen
+  // Reset the screen
   background(25);
-  // change cube proprety
 
-  // recalculate it's point
-  // myCube.translateItem(new Vect(0.01,0,0));
-  // myCube.scaleItem(new Vect(0.999, 0.999, 0.999),  myCube.center );
-  // myCube.rotateItem( new Vect(0.005, 0.005, 0.005), myCube.center );
+  // myCube.translateItem(new Vect(0.001,0.001,0.001), myCube.center);
+  // mySphere.translateItem(new Vect(0.001,0.001,0.001), mySphere.center);
+  // myCylinder.translateItem(new Vect(0.001,0.001,0.001), myCylinder.center);
+  // myCone.translateItem(new Vect(0.001,0.001,0.001), myCone.center);
+
   
-  // mySphere.scaleItem(new Vect(0.999, 0.999, 0.999), mySphere.center );
-  // mySphere.rotateItem( new Vect(0.005, 0.005, 0.005), mySphere.center );
+  // myCube.scaleItem(new Vect(0.999,0.999,0.999), myCube.center);
+  // mySphere.scaleItem(new Vect(0.999,0.999,0.999), mySphere.center);
+  // myCylinder.scaleItem(new Vect(0.999,0.999,0.999), myCylinder.center);
+  // myCone.scaleItem(new Vect(0.999,0.999,0.999), myCone.center);
 
-  // draw the cube
-  // myCube.draw();
+
+  myCube.rotateItem( new Vect(0.005, 0.005, 0.005), myCube.center );
+  mySphere.rotateItem( new Vect(0.005, 0.005, 0.005), mySphere.center );
+  myCylinder.rotateItem( new Vect(0.005, 0.005, 0.005), myCylinder.center );
+  myCone.rotateItem( new Vect(0.005, 0.005, 0.005), myCone.center );
+
+  strokeWeight(2);
+  myCube.draw();
   mySphere.draw();
+  myCylinder.draw();
+  myCone.draw();
 }
